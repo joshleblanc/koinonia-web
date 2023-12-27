1df28501011c0f24761c774f20bbe5eec9f24dbe
Joshua LeBlanc <jleblanc@hey.com>
2023-12-27 18:31:57 -0400

    fix: respond to registrations with json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
+  
   resources :games do 
     resources :game_versions, shallow: true
   end
