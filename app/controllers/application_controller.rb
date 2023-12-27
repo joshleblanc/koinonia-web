e0e9f5e10c35593d553bf1e32799b87893518325
Joshua LeBlanc <jleblanc@hey.com>
2023-12-27 19:19:13 -0400

    fix: override current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
             super
         end
     end
+
+    def current_user
+        @current_user ||= if doorkeeper_token
+                            User.find(doorkeeper_token.resource_owner_id)
+                          else
+                            warden.authenticate(scope: :user)
+                          end
+      end
 end
