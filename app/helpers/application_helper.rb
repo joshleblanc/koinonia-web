ebe2660b51aa8c95e00d33da3c010292237c7034
Joshua LeBlanc <jleblanc@hey.com>
2024-07-24 13:56:28 -0300

    fix: delete button was missing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,39 +1,39 @@
 module ApplicationHelper
   def themes
     [
-          "light",
-          "dark",
-          "cupcake",
-          "bumblebee",
-          "emerald",
-          "corporate",
-          "synthwave",
-          "retro",
-          "cyberpunk",
-          "valentine",
-          "halloween",
-          "garden",
-          "forest",
-          "aqua",
-          "lofi",
-          "pastel",
-          "fantasy",
-          "wireframe",
-          "black",
-          "luxury",
-          "dracula",
-          "cmyk",
-          "autumn",
-          "business",
-          "acid",
-          "lemonade",
-          "night",
-          "coffee",
-          "winter",
-          "dim",
-          "nord",
-          "sunset",
-        ]
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ]
   end
 
   def link_to(name = nil, options = nil, html_options = nil, &block)
@@ -41,8 +41,8 @@ module ApplicationHelper
   end
 
   def route_for_hero(controller, action)
-    Rails.application.routes.routes.select do 
-      _1.defaults[:controller] == controller && _1.defaults[:action] == action
+    Rails.application.routes.routes.select do
+      _1.defaults[:controller] == controller && _1.defaults[:action] == action.to_s
     end.first
   end
 
@@ -55,21 +55,21 @@ module ApplicationHelper
     instance_variable_get(:"@#{params[:controller].singularize}")
   end
 
-  def hero_models 
+  def hero_models
     instance_variable_get(:"@#{params[:controller].pluralize}")
   end
 
   def hero_model_class
     hero_model.class
   end
-  
+
   # let me be the first to say that this is hacky af
   def url_for_hero(params, action)
     controller = params[:controller]
     parts = route_parts(controller, action.to_s)
     obj = {
       controller: controller,
-      action: action
+      action: action,
     }
     parts&.each { obj[_1] = hero_model&.send(_1) || params[_1] }
     obj
